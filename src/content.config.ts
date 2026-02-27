d01a477f4a8b14f6ca8c5340eddb18ef33bee7b7
crow-fox <surakamiya198@gmail.com>
2026-02-27 09:40:27 +0900

    contentCollectionでmockと本物のfetchを切り替え

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,80 +1,25 @@
 import { defineCollection } from "astro:content";
-import { z } from "astro/zod";
-import { microcmsClient } from "./libs/microcms";
+import { fetchAllAreas, fetchAllCafes } from "./features/cafe/fetcher";
+import { getMockAreas, getMockCafes } from "./features/cafe/mock";
+import {
+  type Area,
+  areaSchema,
+  type Cafe,
+  cafeSchema,
+} from "./features/cafe/model";
 
-const microcmsAreaSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  slug: z.string(),
-  priority: z.number(),
-});
-
-const areaSchema = microcmsAreaSchema;
-
-const microcmsCafeSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  slug: z.string(),
-  image: z
-    .object({
-      url: z.string(),
-      width: z.number(),
-      height: z.number(),
-    })
-    .optional(),
-  area: areaSchema,
-  address: z.string(),
-  schedule: z.string(),
-  host: z.string(),
-  contact: z.string().optional(),
-});
-
-// microCMSからのデータが都合が悪い部分のみ、必要な形に変換するためのschema
-const cafeSchema = microcmsCafeSchema.transform((cafe) => ({
-  id: cafe.id,
-  slug: cafe.slug,
-  name: cafe.name,
-  image: cafe.image
-    ? {
-        src: cafe.image.url,
-        width: cafe.image.width,
-        height: cafe.image.height,
-      }
-    : null,
-  area: cafe.area,
-  address: cafe.address,
-  schedule: cafe.schedule,
-  host: cafe.host,
-  contact: cafe.contact ?? "", // contactがnullの場合は空文字を設定
-}));
+const isMockMode = import.meta.env.MODE === "mock";
 
 const areasCollection = defineCollection({
-  loader: async () => {
-    const { contents } = await microcmsClient.getList<unknown>({
-      endpoint: "areas",
-      queries: {
-        fields: "id,name,slug,priority",
-        orders: "priority",
-        limit: 100, // 全部取得するために、最大値の100を指定
-        offset: 0,
-      },
-    });
-    return contents;
+  loader: async (): Promise<Area[]> => {
+    return isMockMode ? getMockAreas() : await fetchAllAreas();
   },
   schema: areaSchema,
 });
 
 const cafesCollection = defineCollection({
-  loader: async () => {
-    const { contents } = await microcmsClient.getList<unknown>({
-      endpoint: "cafes",
-      queries: {
-        fields: "id,name,slug,image,area,address,schedule,host,contact",
-        limit: 100, // 全部取得するために、最大値の100を指定
-        offset: 0,
-      },
-    });
-    return contents;
+  loader: async (): Promise<Cafe[]> => {
+    return isMockMode ? getMockCafes() : await fetchAllCafes();
   },
   schema: cafeSchema,
 });
